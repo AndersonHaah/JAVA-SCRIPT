cc1f83bb5d50f95246d56d3cd89b73e96f88035b
AndersonHaah <Andersonhaah1@gmail.com>
2025-05-28 14:19:58 -0300

    finalizando o projeto de horas com JS

---
+++ ex012/exjs001/script.js
@@ -0,0 +1,18 @@
+function carregar () {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('img#imagem')  
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora <= 12) {
+        img.src = 'imagem-manha.png'
+        document.body.style.background = '#E5E3D6'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'imagem-tarde.png'
+        document.body.style.background = '#BF5B04'
+    } else {
+        img.src = 'imagem-noite.png'
+        document.body.style.background = '#023059'
+    }
+}
